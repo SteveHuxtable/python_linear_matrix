20be7033b67a9d5a535f241eb3ee7721bb6950c2
Steve Hu <stevehu@StevedeMBP.lan>
2020-07-01 21:57:25 +0800

    20200701

--- python-data-analysis.py
+++ python-data-analysis.py
@@ -17,3 +17,32 @@ for name, group in df.groupby('key1'):
     print(group)
 
 df.dtypes
+
+# data aggregation
+df = pd.DataFrame({'data1': np.random.randn(5),
+                   'data2': np.random.randn(5),
+                   'key1': ['a', 'a', 'b', 'b', 'a'],
+                   'key2': ['one', 'two', 'one', 'two', 'one']})
+
+grouped = df.groupby('key1')
+grouped['data1'].quantile(0.9)
+grouped_data1 = grouped['data1']
+grouped_data1.quantile(0.9)
+grouped_data1.mean()
+grouped_data1.agg('mean')
+grouped_data1.agg('sum')
+
+def peak_to_peak(arr):
+    return arr.max() - arr.min()
+
+grouped.agg(peak_to_peak)
+grouped.quantile(0.9)
+grouped.describe()
+
+tips = pd.read_csv('tips.csv')
+tips['tip_pct'] = tips['tip'] / tips['total_bill']
+grouped = tips.groupby(['day', 'smoker'])
+
+grouped_pct = grouped['tip_pct']
+grouped_pct.mean()
+
